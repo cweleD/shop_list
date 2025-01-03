08d629416cdbd20cfec01af33dd6d7f320e11bae
delani <delan.cwele@gmail.com>
2025-01-03 13:31:58 +0200

    Shopping form mark up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="shopping">
+      <h1>Shopping Budget</h1>
+      <Budget />
+      <Form />
+      <ProductList />
     </div>
   );
 }
 
-export default App;
+function Budget() {
+  return (
+    <>
+      <form className="form-group">
+        <label>Budget: </label>
+        <input type="text" />
+        <button>Add</button>
+      </form>
+      <p>Your budget is: R5000, shopping can be fun but please don't exceed!</p>
+    </>
+  );
+}
+
+function Form() {
+  return (
+    <form>
+      <div className="form-group">
+        <div className="input-field">
+          <label>Product: </label>
+          <input type="text" />
+        </div>
+        <div className="inputField">
+          <label>price: </label>
+          <input type="text" />
+        </div>
+        <button>Add</button>
+      </div>
+    </form>
+  );
+}
+
+function ProductList() {
+  return (
+    <ul>
+      <Product />
+    </ul>
+  );
+}
+
+function Product() {
+  return (
+    <>
+      <li>
+        <span className="item">Product One</span>
+        <span className="price">R450</span>
+        <button>Delete</button>
+      </li>
+      <li>
+        <span className="item">Product One</span>
+        <span className="price">R450</span>
+        <button>Delete</button>
+      </li>
+      <li>
+        <span className="item">Product One</span>
+        <span className="price">R450</span>
+        <button>Delete</button>
+      </li>
+      <li>
+        <span className="item">Product One</span>
+        <span className="price">R450</span>
+        <button>Delete</button>
+      </li>
+      <li>
+        <span className="item">Product One</span>
+        <span className="price">R450</span>
+        <button>Delete</button>
+      </li>
+    </>
+  );
+}
